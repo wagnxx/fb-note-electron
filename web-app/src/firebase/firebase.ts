e890d10439d42e40e66dc972eb6fc3eaafa0f73e
Mr WANG <2362999724@qq.com>
2024-12-28 21:32:22 +0800

    Add an environment configuration file for the web app.

---
+++ web-app/src/firebase/firebase.ts
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app'
+import { getStorage } from 'firebase/storage'
+
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+  REACT_APP_FIREBASE_MEASUREMENT_ID,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  // eslint-disable-next-line no-undef
+} = process.env
+
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+export const fbStorage = getStorage(app)
